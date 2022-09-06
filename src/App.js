33746dc69072c0c04e365f6c267538d6aa55822b
youngjerald239 <youngjerald239@gmail.com>
2022-09-06 10:08:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const menuItems = [
+   {name: "Angus Burger", price: 8.99, category: 'burger'},
+   {name: "Tuna Steak Burger", price: 15.00, category: 'burger'},
+   {name: "Bacon Burger", price: 11.50, category: 'burger'},
+   {name: "Southwest Chicken Burger", price: 9.99, category: 'burger'},
+   {name: "Mozzarella Burger", price: 12.50, category: 'burger'},
+   {name: "Cesar Salad", price: 6.50, category: 'salad'},
+   {name: "BBQ Chicken Salad", price: 13.99, category: 'salad'},
+   {name: "Garden Salad", price: 9.99, category: 'salad'},
+   {name: "Veggie Lasagna", price: 17.99, category: 'pasta'},
+   {name: "Spaghetti & Meatballs", price: 17.99, category: 'pasta'},
+   {name: "Fettuccine Alfredo", price: 17.99, category: 'pasta'},
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {menuItems.map(menuItem => (
+        <li key={menuItem.name}>{menuItem.name} - ${menuItem.price} - {menuItem.category}</li>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
