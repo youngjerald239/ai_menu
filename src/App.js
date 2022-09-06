9694cde94d26db4666b4bb205bef19dd0d1db82c
youngjerald239 <youngjerald239@gmail.com>
2022-09-06 11:28:29 -0500

    added alan to page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-const menuItems = [
-   {name: "Angus Burger", price: 8.99, category: 'burger'},
-   {name: "Tuna Steak Burger", price: 15.00, category: 'burger'},
-   {name: "Bacon Burger", price: 11.50, category: 'burger'},
-   {name: "Southwest Chicken Burger", price: 9.99, category: 'burger'},
-   {name: "Mozzarella Burger", price: 12.50, category: 'burger'},
-   {name: "Cesar Salad", price: 6.50, category: 'salad'},
-   {name: "BBQ Chicken Salad", price: 13.99, category: 'salad'},
-   {name: "Garden Salad", price: 9.99, category: 'salad'},
-   {name: "Veggie Lasagna", price: 17.99, category: 'pasta'},
-   {name: "Spaghetti & Meatballs", price: 17.99, category: 'pasta'},
-   {name: "Fettuccine Alfredo", price: 17.99, category: 'pasta'},
-];
+import alanBtn from '@alan-ai/alan-sdk-web'
 
 function App() {
 
   const [cart, setCart] = useState([])
+  const [menuItems, setMenuItems] = useState([])
+
+  useEffect(() => {
+  alanBtn({
+      key: 'b85ff7bc8eefa99733b28c0eca2c6acd2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData) => {
+        if(commandData.command === 'getMenu'){
+          setMenuItems(commandData.data)
+        }
+      }
+  });
+}, []);
 
   const addToCart = (menuItem) => {
     setCart((oldCart) => {
